8371549ba204ca5970182515c6ed197a39ab5f55
JohnMurphy00 <jmurphy@utcmediacityuk.org.uk>
2016-09-13 10:46:11 +0100

    Add files via upload

---
+++ AlwaysMakeANewProject.java
@@ -0,0 +1,51 @@
+
+package always.make.a.pkgnew.project;
+
+
+public class AlwaysMakeANewProject {
+
+   
+    public static void main(String[] args) {
+        
+        String Colour = "Orange";
+        String Street = "North Drive";
+        String Name = "John";
+        
+        System.out.println(Colour);
+        System.out.println(Street);
+        System.out.println(Name);
+        
+        int Number = 5;
+        int Age = 16;
+        int Shoe = 10;
+        
+        System.out.println("Your chosen number was: " + Number);
+        System.out.println("Your age is: " + Age);
+        System.out.println("Your shoe size is: " + Shoe);
+        
+        double Test = 1.5;
+        double Pi = 3.141;
+        
+        System.out.println(Pi);
+        System.out.println(Test);
+        //area of a circle = pi*r^2
+        //speed = distance/time
+        
+        //"*" for multiplication "/" for division
+        System.out.println(Age + Shoe);
+        System.out.println(Age - Shoe);
+        System.out.println(Age * Shoe);
+        System.out.println(Age / Shoe);
+        
+        int x = 10;
+        int y = 5;
+        
+        if(x < y){
+            System.out.println("x is less than y");
+        }else{
+            System.out.println("x is more than y");
+        }
+            
+        
+    
+    }
